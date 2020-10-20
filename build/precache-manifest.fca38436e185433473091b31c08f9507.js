b891b9378b9ef296f1116614804d83fe20571957
Connor Watumull <connor.watumull@gmail.com>
2020-10-20 14:43:51 -0700

    build

---
+++ build/precache-manifest.fca38436e185433473091b31c08f9507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428a3b29c7368d8b24c3fb1398da9028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c479ef598d0b8e9c7b3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ea8955eb7a2995296a7",
+    "url": "/static/js/2.abccf7d8.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.abccf7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c479ef598d0b8e9c7b3",
+    "url": "/static/js/main.59152ff4.chunk.js"
+  },
+  {
+    "revision": "526d3b737eb9cb5265f2",
+    "url": "/static/js/runtime-main.ba3c5f1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
